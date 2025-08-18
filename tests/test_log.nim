4b4e53be726afede38655c0c4dabb2368b323924
Marto <martindobrev0@gmail.com>
2025-08-18 18:45:25 +0300

    Add entry log tests

--- tests/test_log.nim
+++ tests/test_log.nim
@@ -121,33 +121,39 @@ suite "RaftLog Tests":
     check log.lastIndex == RaftLogIndex(2)
     check log.entriesCount == 2
 
-  # test "getEntryByIndex returns entry for valid index":
-  #   var snapshot =
-  #     RaftSnapshot(index: RaftLogIndex(0), term: RaftNodeTerm(0), config: RaftConfig())
-  #   var entries =
-  #     @[LogEntry(
-  #       term: RaftNodeTerm(1),
-  #       index: RaftLogIndex(1),
-  #       kind: RaftLogEntryType.rletCommand,
-  #       command: Command(data: @[]),
-  #     )]
+  test "checkInvariant validates sequential log and config indices":
+    var snapshot =
+      RaftSnapshot(index: RaftLogIndex(0), term: RaftNodeTerm(1), config: RaftConfig())
+    var log = RaftLog.init(snapshot)
+    log.appendAsLeader(
+      term = RaftNodeTerm(1),
+      index = RaftLogIndex(1),
+      data = Command(data: @[]),
+    )
+    log.appendAsLeader(
+      term = RaftNodeTerm(1),
+      index = RaftLogIndex(2),
+      data = Command(data: @[]),
+    )
+    log.checkInvariant()
 
-  #   var log = RaftLog.init(snapshot, entries)
-  #   let entryOpt = log.getEntryByIndex(RaftLogIndex(1))
-  #   check entryOpt.isSome
-  #   check entryOpt.get().index == RaftLogIndex(1)
-
-  # test "getEntryByIndex returns none for out-of-bounds index":
-  #   var snapshot =
-  #     RaftSnapshot(index: RaftLogIndex(0), term: RaftNodeTerm(0), config: RaftConfig())
-  #   var entries =
-  #     @[LogEntry(
-  #       term: RaftNodeTerm(1),
-  #       index: RaftLogIndex(1),
-  #       kind: RaftLogEntryType.rletCommand,
-  #       command: Command(data: @[]),
-  #     )]
-
-  #   var log = RaftLog.init(snapshot, entries)
-  #   check log.getEntryByIndex(RaftLogIndex(0)).isNone
-  #   check log.getEntryByIndex(RaftLogIndex(2)).isNone
+  test "checkInvariant detects gaps":
+    var snapshot =
+      RaftSnapshot(index: RaftLogIndex(0), term: RaftNodeTerm(1), config: RaftConfig())
+    var entries = @[
+      LogEntry(
+        term: RaftNodeTerm(1),
+        index: RaftLogIndex(1),
+        kind: RaftLogEntryType.rletCommand,
+        command: Command(data: @[]),
+      ),
+      LogEntry(
+        term: RaftNodeTerm(1),
+        index: RaftLogIndex(3),
+        kind: RaftLogEntryType.rletCommand,
+        command: Command(data: @[]),
+      ),
+    ]
+    var log = RaftLog.init(snapshot, entries)
+    expect AssertionError:
+      log.checkInvariant()
